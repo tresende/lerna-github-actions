591de00b98414197f89a5147fa1b0733b675527c
Thiago Resende <thiago.gcresende@gmail.com>
2021-11-12 20:27:40 -0300

    test: add module paths

--- packages/main/jest.config.js
+++ packages/main/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?'],
   setupFilesAfterEnv: ['<rootDir>/../../.jest/setup.ts'],
-  // modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   moduleNameMapper: {
     // '^styled-components': '../<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
   },
